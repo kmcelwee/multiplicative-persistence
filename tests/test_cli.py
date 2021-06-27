41d9588c054c1911a6df3b5bc85cb640352e8d05
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-06-27 11:40:35 -0400

    Add CLI testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,20 @@
 from typer.testing import CliRunner
-
+import os
 from cli import app
 
 runner = CliRunner()
 
 
-def test_app():
-    result = runner.invoke(app, ["--start", "3", "--end", "5"])
+def test_search():
+    start = 3
+    end = 5
+    TEST_OUTPUT_PATH = "tests/output"
+    expected_output = os.path.join(TEST_OUTPUT_PATH, f"{start}-{end}.json")
+    os.remove(expected_output)
+    assert not os.path.isfile(expected_output)
+
+    result = runner.invoke(
+        app, ["--start", start, "--end", end, "--output-dir", TEST_OUTPUT_PATH]
+    )
     assert result.exit_code == 0
+    assert os.path.isfile(expected_output)
