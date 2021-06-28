444fcba83110113337fd77611375c504083b37ab
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-06-27 20:36:57 -0400

    Add elements to tree

--- MultiplicativePersistence/Tree.py
+++ MultiplicativePersistence/Tree.py
@@ -1,4 +1,8 @@
 """
+TODO
+- collect helpful functions from variant and here into a utils file
+- change tree -> trees
+
 
     methods
         build_tree
@@ -8,7 +12,54 @@
 
 """
 
+from anytree import Node, RenderTree
+from functools import reduce
+
+from MultiplicativePersistence import MpNumberVariant
+
 
 class Tree:
     def __init__(self, collection):
         self.collection = collection
+        self.node_dict = {x: Node(x) for x in range(0, 10)}
+
+    def get_node(self, num):
+        if num not in self.node_dict:
+            raise Exception(f"The number {num} does not appear in this tree")
+        return self.node_dict[num]
+
+    def add_child(self, child_num):
+        mp_list = self.get_mp_list(child_num)
+        parent_node = self.get_node(mp_list[0])
+        for num in mp_list[1:]:
+            if num in self.node_dict:
+                parent_node = self.get_node(num)
+                continue
+
+            new_node = Node(num, parent=parent_node)
+            self.node_dict[num] = new_node
+
+            parent_node = new_node
+
+    def build_node_dict(self):
+        node_dict = {}
+        variants = self.collection.all_variants()
+
+        for variant in variants:
+            self.add_node_and_children(variant.base_10)
+
+        return node_dict
+
+    @classmethod
+    def get_number_child(self, n):
+        int_list = [int(x) for x in str(n)]
+        return reduce((lambda x, y: x * y), int_list)
+
+    @classmethod
+    def get_mp_list(self, num):
+        return_list = [num]
+        this_num = num
+        while len(str(this_num)) != 1:
+            this_num = self.get_number_child(this_num)
+            return_list.append(this_num)
+        return list(reversed(return_list))
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,3 +1,5 @@
+from anytree import Node, RenderTree
+
 from MultiplicativePersistence import Tree, MpNumberCollection
 
 
@@ -6,3 +8,32 @@ class TestTree:
         collection = MpNumberCollection(json_path="tests/fixtures/5-10.json")
         tree = Tree(collection)
         assert tree.collection == collection
+
+    def test_get_number_child(self):
+        assert Tree.get_number_child(81) == 8
+        assert Tree.get_number_child(2231) == 12
+        assert Tree.get_number_child(88341) == 768
+
+    def test_get_mp_list(self):
+        assert Tree.get_mp_list(18432) == [8, 18, 192, 18432]
+        assert Tree.get_mp_list(0) == [0]
+
+    def test_add_child(self):
+        collection = MpNumberCollection(json_path="tests/fixtures/5-10.json")
+        tree = Tree(collection)
+        tree.add_child(15)
+        five_root = tree.get_node(5)
+        assert five_root.children[0].name == 15
+
+        tree.add_child(1176)
+        eight_root = tree.get_node(8)
+        assert eight_root.children[0].name == 42
+
+        tree.add_child(177147)
+
+        implicit_node = tree.get_node(1372)
+        assert implicit_node.name == 1372
+        assert implicit_node.parent.name == 42
+
+        root_42 = tree.get_node(42)
+        assert len(root_42.children) == 2
